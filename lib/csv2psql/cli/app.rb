5d38c2a8bef81ecc363b772e9b2ec860ba4b12ea
Tomas Korcak <korczis@gmail.com>
2014-09-07 20:56:03 -0700

    Remove redundant code

--- lib/csv2psql/cli/app.rb
+++ lib/csv2psql/cli/app.rb
@@ -15,52 +15,6 @@ end
 
 include GLI::App
 
-program_desc "csv2psql #{Csv2Psql::VERSION} (Codename: #{Csv2Psql::CODENAME})"
-
-cmds = {
-  h: {
-    desc: 'Header row included',
-    default_value: Csv2Psql::Processor::DEFAULT_OPTIONS['header']
-  },
-
-  d: {
-    desc: 'Column delimiter',
-    type: String,
-    default_value: Csv2Psql::Processor::DEFAULT_OPTIONS['delimiter']
-  },
-
-  l: {
-    desc: 'How many rows process',
-    type: Integer,
-    default_value: -1
-  },
-
-  q: {
-    desc: 'Quoting character',
-    type: String,
-    default_value: Csv2Psql::Processor::DEFAULT_OPTIONS['quote']
-  },
-
-  s: {
-    desc: 'Line separator',
-    type: String,
-    default_value: Csv2Psql::Processor::DEFAULT_OPTIONS['separator']
-  },
-
-  'skip' => {
-    desc: 'How many rows skip',
-    type: Integer,
-    default_value: -1
-  }
-}
-
-switch [:h, :header], cmds[:h]
-flag [:d, :delimiter], cmds[:d]
-flag [:l, :limit], cmds[:l]
-flag [:q, :quote], cmds[:q]
-flag [:s, :separator], cmds[:s]
-flag [:skip], cmds['skip']
-
 module Csv2Psql
   # Apollon CLI
   module Cli
